91625c0f0b93e8de8911fa822abb4c9a7dd4c1ca
VincentHChoy <vhchoy@ualberta.ca>
2022-04-18 18:53:42 +0000

    added a template for urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -25,6 +25,12 @@ app.get("/urls", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL)
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.slice(0,-1)] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
