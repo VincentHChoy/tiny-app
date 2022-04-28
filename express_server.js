26338b565069f8b05cce86087454641db59326b7
VincentHChoy <vhchoy@ualberta.ca>
2022-04-28 04:25:50 +0000

    added console log and link to page on command line interface

--- express_server.js
+++ express_server.js
@@ -177,5 +177,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny App listening on port ${PORT}! \nTiny App : http://localhost:8080/`);
 });
